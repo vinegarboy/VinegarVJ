5d1675adeae2f869014358c99417cdebe3bbc8c6
vinegarboy <necogusa398@gmail.com>
2024-09-13 16:01:12 +0900

    スクリプト関係の作成とPrefabの作成

--- Assets/Scripts/ChannelClass.cs
+++ Assets/Scripts/ChannelClass.cs
@@ -17,10 +17,19 @@ public class Channel{
         }
         video = VideoManager.selectObj;
         video_id = video.guid;
+        ChangeParentMaterial();
     }
 
     public void setVideo(VideoObject video){
         this.video = video;
         video_id = video.guid;
+        ChangeParentMaterial();
+    }
+
+    private void ChangeParentMaterial(){
+        if(video == null){
+            return;
+        }
+        ChannelManager.material.SetTexture("_Tex" + channel_id+1, video.renderTexture);
     }
 }
---
+++ Assets/Scripts/FileScript.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using TMPro;
+
+public class FileScript : MonoBehaviour{
+    public string path;
+    public int bpm;
+    public bool has_bpm = true;
+    [SerializeField] private Image image;
+    [SerializeField] private GameObject BackImagePanel;
+    [SerializeField] private TextMeshProUGUI FileName;
+    [SerializeField] private TextMeshProUGUI BPM;
+
+    private VideoObject video;
+
+    public void Initialize(){
+        if(path == null){
+            Debug.Log("Path is null");
+            return;
+        }
+        video = new VideoObject(path);
+        if(has_bpm){
+            video.bpm = bpm;
+            BPM.text = video.bpm.ToString();
+        }else{
+            BPM.text = "No BPM";
+        }
+        FileName.text = Path.GetFileName(path);
+        image.material = video.Prev_material;
+    }
+
+    void Update(){
+        if(VideoManager.selected){
+            if(VideoManager.selectObj.guid != video.guid){
+                BackImagePanel.SetActive(false);
+            }
+        }
+    }
+
+    public void OnClick(){
+        BackImagePanel.SetActive(true);
+        VideoManager.selectObj = video;
+        VideoManager.selected = true;
+    }
+}
