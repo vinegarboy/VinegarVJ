0082719c6bdcbf0f28ddaebdee4882c015938a29
vinegarboy <necogusa398@gmail.com>
2025-06-14 21:59:26 +0900

    アップデート時にプロジェクト内容が消えたので追加。 ページマネージャーとチャンネルスクリプトの作成

--- Assets/Scripts/PageManager.cs
+++ Assets/Scripts/PageManager.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PageManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class PageManager : MonoBehaviour{
+	//ページングするPrefabを指定
+	[SerializeField]
+	private GameObject[] pagePrefabs;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//現在のページ番号
+	private int currentPageIndex = 0;
+
+	void Awake(){
+		(this.transform as RectTransform).sizeDelta = new Vector2(Screen.width * 2, Screen.height);
+		currentPageIndex = 0;
+	}
+
+	void ToRight(){
+		currentPageIndex++;
+	}
+
+	void ToLeft(){
+		currentPageIndex--;
+	}
+
+	void SetPagePosition(){
+		pagePrefabs[currentPageIndex].transform.localPosition = new Vector3(0, 0, 0);
+	}
 }
